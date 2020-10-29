99c1ed46c436981bb2c2b3e990fff1b6b56078a7
JBatRey <59271764+JBatRey@users.noreply.github.com>
2020-10-29 11:01:44 -0300

    Update puntoFijo.py

--- 3-EcusNL/puntoFijo.py
+++ 3-EcusNL/puntoFijo.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+#buscamos x = g(x) 
 #El 0 tiene que estar encuadrado en [a,b]
 #g(x) pertenece a [a,b] si x pertenece a [a,b]
 # la derivada de g(x) en [a,b] es MENOR a 1
 
 def g(x):
   return np.exp(x)
-x = 0.2
 
+x = 0.2
 for n in range(300):
   x = g(x)
 
